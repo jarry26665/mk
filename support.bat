6f605931c86a66bfa50e660d00c7c9602c67b25b
jarry26665 <30381140+jarry26665@users.noreply.github.com>
2021-06-06 15:37:53 +0500

    Add files via upload

---
+++ support.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+color 0A
+TITLE MK Roots
+echo ===============================================================================================
+echo.
+echo                                                -----------------------
+echo                                  8888ba.88ba  dP     dP  .88888.  .d88888b  8888ba.88ba  
+echo                                  88  `8b  `8b 88   .d8' d8'   `88 88.    "' 88  `8b  `8b 
+echo                                  88   88   88 88aaa8P'  88        `Y88888b. 88   88   88 
+echo                                  88   88   88 88   `8b. 88   YP88       `8b 88   88   88 
+echo                                  88   88   88 88     88 Y8.   .88 d8'   .8P 88   88   88 
+echo                                  dP   dP   dP dP     dP  `88888'   Y88888P  dP   dP   dP
+echo                                                -----------------------
+echo                                                TOOL BY Mehran Khan
+echo                                               Prazanted By  HalabTech
+echo.
+echo                                                 How May I Help You 
+echo.
+echo [*] Press Enter To Follo Us And Text Mk Gsm To Add Your Model Root
+echo.
+pause
+start https://www.facebook.com/rootbymk/?ref=pages_you_manage
+start https://www.youtube.com/channel/UCqDlZbhFvxY5NdWengm6ZGA
+exit

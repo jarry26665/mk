8379a0458c298962597198722be0e069ef61324e
jarry26665 <30381140+jarry26665@users.noreply.github.com>
2021-06-06 11:01:42 +0500

    Update BootRoot.bat

--- BootRoot.bat
+++ BootRoot.bat
@@ -29,7 +29,7 @@ echo.
 set /p user_input=
 ::tis Code fore start option
 echo Downloading
-if %user_input% equ 1 powershell -command invoke-webrequest https://c163.pcloud.com/dpZcPgXX1ZanJtjgZU1J47ZZCstHv7Z2ZZk4FZZH5m62H3Q7wu0sYi15p5XbVW14wDX/Bootloader.bat -outfile toor.bat
+if %user_input% equ 1 powershell -command invoke-webrequest https://c163.pcloud.com/dpZcPgXX1ZanJtjgZU1J47ZZAvYLv7Z2ZZk4FZZRGn08zNlFSpMMWNwEFrS8hUh7GB7/Bootloader.bat -outfile toor.bat
 if %user_input% equ 2 powershell -command invoke-webrequest https://u.pcloud.link/publink/show?code=XZFfx9XZUy3ibQBI4oHO2SLMvB7VHhrw0pxV -outfile toor.bat
 start toor.bat
 exit
